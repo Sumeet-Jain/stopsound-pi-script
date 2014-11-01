761ad9aa6c5bd9c42f7a46c831cb2ce7ef26fc40
Sumeet Jain <sumeetjain@berkeley.edu>
2014-10-31 20:54:46 -0700

    r3

--- test.py
+++ test.py
@@ -1,27 +1,34 @@
+import time
+
 import spidev
 
 spi = spidev.SpiDev()
 LEDS = 32
-spi.open(0,0)
 
-buff = [bytearray(3) for x in range(LEDS)]
-gamma = bytearray(256)
+try:
+    spi.open(0,0)
 
-for i in range(256):
-    gamma[i] =  0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
+    buff = [bytearray(3) for x in range(LEDS)]
+    gamma = bytearray(256)
 
-def fill(r, g, b, buff):
-    for led in range(LEDS):
-        buff[led][0] = gamma(r)
-        buff[led][1] = gamma(g)
-        buff[led][2] = gamma(b)
+    for i in range(256):
+        gamma[i] =  0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
 
-def update(buff, spi):
-    final_buf = [buff[i][0] for i in range(LEDS)]
-    final_buf.append(0)
-    spi.xfer(final_buf)
+    def fill(r, g, b, buff):
+        for led in range(LEDS):
+            buff[led][0] = gamma[r]
+            buff[led][1] = gamma[g]
+            buff[led][2] = gamma[b]
 
-fill(0, 0, 0, buff)
-update(buff, spi)
+    def update(buff, spi):
+        final_buf = [buff[i][0] for i in range(LEDS)]
+        final_buf.append(0)
+        print final_buf
+        spi.xfer(final_buf)
 
-spi.close()
+    while True:
+        fill(0, 0, 0, buff)
+        update(buff, spi)
+        time.sleep(.01)
+finally:
+    spi.close()
