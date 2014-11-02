eb5314f8de83f8e36d8018f48c4c35254f1dca1e
Sumeet Jain <sumeetjain@berkeley.edu>
2014-11-01 23:41:25 -0700

    refactored login code

--- sound_decector.py
+++ sound_decector.py
@@ -14,23 +14,23 @@ try:
 except ImportError:
     spidev = None
 
-LOGGING = False
+def logger():
+    LOGGING = False
+    def inner(string):
+        if LOGGING:
+            print string
+    return inner
 
-def log(string):
-    if LOGGING:
-        print string
+log = logger()
 
 
 CHUNK_SIZE = 512
 FORMAT = pyaudio.paInt16
 RATE = 44100
+
 TIME_TO_RESPOND = 5
 AMBIENT_SOUND_TIME = 10
 QUIET_REST_TIME = .5
-
-STOPSOUND_URL = 'http://stopsound.herokuapp.com/'
-CONTACTS_URL = STOPSOUND_URL + 'contacts/get_actives/'
-LOGIN_URL = STOPSOUND_URL + 'auth/login/'
 
 LEDS = 32
 
@@ -56,6 +56,53 @@ class LightsController(object):
         final_buff.append(0)
         spi.xfer(final_buff)
 
+class StopSoundSite(object):
+
+    def __init__(self, creds):
+        self.STOPSOUND_URL = 'http://stopsound.herokuapp.com/'
+        self.CONTACTS_URL = self.STOPSOUND_URL + 'contacts/get_actives/'
+        self.LOGIN_URL = self.STOPSOUND_URL + 'auth/login/'
+        self.SETTING_URLS = self.STOPSOUND_URL + 'contacts/view_settings/'
+        self.CREDS = creds
+
+    @contextmanager
+    def login(self):
+        with requests.Session() as sesh:
+            login_page = sesh.get(self.LOGIN_URL)
+
+            # Grab csrf token from the form
+            soup = BeautifulSoup(login_page.text)
+            for input_ in soup.find_all('input'):
+                if 'name' in input_.attrs and input_.attrs['name'] == 'csrfmiddlewaretoken':
+                    token = input_['value']
+                    break;
+
+            login_data = {}
+            login_data['csrfmiddlewaretoken'] =  token
+            login_data['username'] = self.CREDS['stopsound_username']
+            login_data['password'] = self.CREDS['stopsound_password']
+            login_resp = sesh.post(self.LOGIN_URL, data=login_data)
+            assert(login_resp.status_code == 200)
+
+            yield sesh
+
+    def send_messages(self):
+        with self.login() as sesh:
+            voice = Voice()
+            voice.login(email=self.CREDS['googlevoice_email'], passwd=self.CREDS['googlevoice_password'])
+            contacts = sesh.get(self.CONTACTS_URL)
+            for name, number in contacts.json().items():
+                print( "sending a message to %s" % name)
+                voice.send_sms(number, "Dear %s, Stop Sound is notifying that you may be too loud. Contact your nearest neighbor." % name)
+                print("finished")
+
+    def get_settings(self):
+        with self.login() as sesh:
+            settings_resp = sesh.get(self.SETTINGS_URL)
+            assert(settings_resp.status_code == 200)
+            return settings_resp.json()
+
+
 @contextmanager
 def spi_opener():
     if spidev:
@@ -79,39 +126,8 @@ def recorder():
     stream.close()
     p.terminate()
 
-def send_messages():
-    with requests.Session() as sesh:
-        login_page = sesh.get(LOGIN_URL)
-
-        # Grab csrf token from the form
-        soup = BeautifulSoup(login_page.text)
-        for input_ in soup.find_all('input'):
-            if 'name' in input_.attrs and input_.attrs['name'] == 'csrfmiddlewaretoken':
-                token = input_['value']
-                break;
-
-        login_data = {}
-        login_data['csrfmiddlewaretoken'] =  token
-        login_data['username'] = CREDS['stopsound_username']
-        login_data['password'] = CREDS['stopsound_password']
-        login_resp = sesh.post(LOGIN_URL, data=login_data)
-        assert(login_resp.status_code == 200)
-
-        voice = Voice()
-        voice.login(email=CREDS['googlevoice_email'], passwd=CREDS['googlevoice_password'])
-
-        contacts = sesh.get(CONTACTS_URL)
-        for name, number in contacts.json().items():
-            print( "sending a message to %s" % name)
-            voice.send_sms(number, "Dear %s, Stop Sound is notifying that you may be too loud. Contact your nearest neighbor." % name)
-            print("finished")
-
-
 def is_loud(sound_data, threshold):
     return max(sound_data) > threshold 
-
-def respond_to_loud_sound():
-    send_messages()
 
 def monitor_sound(threshold, lights, spi):
     with recorder() as stream:
@@ -125,8 +141,8 @@ def monitor_sound(threshold, lights, spi):
                 # little endian, signed short
                 if byteorder == 'big':
                     sound_data.byteswap()
-                now = time.time()
 
+                now = time.time()
                 if is_loud(sound_data, threshold):
                     time_not_hearing = 0
                     time_hearing += now - timestamp
@@ -163,20 +179,22 @@ def get_ambient_threshold():
     ambient_sound = ambient_sound / samples
     return ambient_sound + 50
 
+
 if __name__ == '__main__':
     print "Sound monitor -- Stop Sound -- Nu Pledge Class -- GOOOOO"
     threshold = get_ambient_threshold()
     print "Threshold is currently at %f" % threshold
 
     with open('creds.json', 'r') as f_:
-        CREDS = json.load(f_)
+        creds = json.load(f_)
+        stop_sound_site = StopSoundSite(creds)
 
     with spi_opener() as spi:
         while True:
             print "Monitoring Sound"
             lights = LightsController(LEDS)
             monitor_sound(threshold, lights, spi)
-            send_messages()
+            stop_sound_site.send_messages()
             start = time.time()
             if spi:
                 while time.time() - start < 10:
